7905e4945514d22bdbbe1d556a940429f386c6c8
SAM <sameerkhan27560@gmail.com>
2025-08-21 00:02:37 +0530

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
-import tailwindcss from '@tailwindcss/vite';
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/FlowBoard/', //  important for GitHub Pages
-  plugins: [react(), tailwindcss()],
-});
+  plugins: [react(),
+    tailwindcss(),
+  ],
+})
